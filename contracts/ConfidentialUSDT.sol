b7addf2ea9f14303394a008126bbe294f068e0c4
shannawood <gy90cdcq4fl9a@hotmail.com>
2025-08-15 16:28:46 +0800

    update confidential

--- contracts/ConfidentialUSDT.sol
+++ contracts/ConfidentialUSDT.sol
@@ -4,7 +4,35 @@ pragma solidity ^0.8.24;
 import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
 import {ConfidentialFungibleToken} from "@openzeppelin/confidential-contracts/token/ConfidentialFungibleToken.sol";
 import {SepoliaConfig} from "@fhevm/solidity/config/ZamaConfig.sol";
+import {FHE, euint64} from "@fhevm/solidity/lib/FHE.sol";
 
 contract ConfidentialUSDT is ConfidentialFungibleToken, SepoliaConfig {
-    constructor() ConfidentialFungibleToken("cUSDT", "cUSDT", "") {}
+    address public owner;
+    euint64 public totalSupply;
+    uint256 public constant INITIAL_SUPPLY = 1000000 * 10**6; // 1M USDT with 6 decimals
+    
+    event SupplyMinted(address indexed to, uint256 amount);
+    
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not owner");
+        _;
+    }
+    
+    constructor() ConfidentialFungibleToken("cUSDT", "cUSDT", "") {
+        owner = msg.sender;
+        totalSupply = FHE.asEuint64(uint64(INITIAL_SUPPLY));
+        FHE.allowThis(totalSupply);
+    }
+    
+    function mint(address to, uint64 amount) public onlyOwner {
+        euint64 encryptedAmount = FHE.asEuint64(amount);
+        _mint(to, encryptedAmount);
+        totalSupply = FHE.add(totalSupply, encryptedAmount);
+        FHE.allowThis(totalSupply);
+        emit SupplyMinted(to, amount);
+    }
+    
+    function getTotalSupply() public view returns (euint64) {
+        return totalSupply;
+    }
 }
